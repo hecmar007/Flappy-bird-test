a69d903c7734df960495a4653f1eaf2e9c94bd18
hecmar007 <hecbr@hotmail.es>
2018-01-29 20:57:37 +0100

    Added the project

---
+++ Flappy bird/Assets/Scripts/BirdController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float force = 200f;
+    private Animator anim;
+
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, force));
+                anim.SetTrigger("Flap");
+            }
+        }
+	}
+
+    void OnCollisionEnter2D ()
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        gameController.instance.BirdDied();
+    }
+}
---
+++ Flappy bird/Assets/Scripts/gameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameController : MonoBehaviour {
+
+    public static gameController instance;
+
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
+    public float scrollSpeed = -1.5f;
+
+	// Use this for initialization
+	void Awake ()
+    {
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
